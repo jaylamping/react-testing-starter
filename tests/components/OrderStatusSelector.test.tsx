5fe3cf122d67e10f1076f6d496dc8665493e18c8
jaylamping <35737718+jaylamping@users.noreply.github.com>
2024-08-28 11:06:09 -0500

    refactored OrderStatusSelector

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -5,15 +5,18 @@ import OrderStatusSelector from '../../src/components/OrderStatusSelector';
 
 describe('OrderStatusSelector', () => {
   const renderOrderStatusSelector = () => {
+    const onChange = vi.fn();
     render(
       <Theme>
-        <OrderStatusSelector onChange={vi.fn()} />
+        <OrderStatusSelector onChange={onChange} />
       </Theme>
     );
 
     return {
       button: screen.getByRole('combobox'),
       getOptions: () => screen.findAllByRole('option'),
+      getOption: (label: RegExp) => screen.getByRole('option', { name: label }),
+      onChange,
       user: userEvent.setup(),
     };
   };
@@ -33,5 +36,38 @@ describe('OrderStatusSelector', () => {
     expect(options).toHaveLength(3);
     const labels = options.map((option) => option.textContent);
     expect(labels).toEqual(['New', 'Processed', 'Fulfilled']);
+  });
+
+  it.each([
+    { label: /processed/i, value: 'processed' },
+    { label: /fulfilled/i, value: 'fulfilled' },
+  ])(
+    'should call onChange with $value when the $label option is selected',
+    async ({ label, value }) => {
+      const { button, getOption, onChange, user } = renderOrderStatusSelector();
+
+      await user.click(button);
+
+      const option = getOption(label);
+      await user.click(option);
+
+      expect(onChange).toHaveBeenCalledWith(value);
+    }
+  );
+
+  it("should call onChange with 'new' when the 'New' option is selected", async () => {
+    const { button, getOption, onChange, user } = renderOrderStatusSelector();
+
+    await user.click(button);
+
+    const processedOption = getOption(/processed/i);
+    await user.click(processedOption);
+
+    await user.click(button);
+
+    const newOption = getOption(/new/i);
+    await user.click(newOption);
+
+    expect(onChange).toHaveBeenCalledWith('new');
   });
 });
